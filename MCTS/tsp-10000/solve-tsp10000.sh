2447d63e88b973f0d8c32c9f7cfe6c48baa6cba1
TSP <44106649+Spider-scnu@users.noreply.github.com>
2020-05-16 18:15:26 +0800

    generate_lib

--- MCTS/tsp-10000/solve-tsp10000.sh
+++ MCTS/tsp-10000/solve-tsp10000.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 rm -r TSP.o
 rm -r test
+
 g++ -lstdc++ -std=c++11 -fPIC -fvisibility=hidden -o  TSP.o -c TSP.cpp  -I/usr/local/cuda-9.0/samples/common/inc
 
 g++ -lstdc++ -std=c++11 -fPIC -o test TSP.o kernel.o -L. -L/usr/local/cuda-9.0/lib64/ -lcudart -lcuda -lpthread -lm -ldl
