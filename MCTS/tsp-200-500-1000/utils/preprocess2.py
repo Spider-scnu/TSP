64721f58409319e29b7b8afa77a83123da5550ea
TSP <44106649+Spider-scnu@users.noreply.github.com>
2021-06-02 12:28:10 +0800

    tsp200

---
+++ MCTS/tsp-200-500-1000/utils/preprocess2.py
@@ -0,0 +1,14 @@
+with open('./kernel.h', 'r') as f:
+    lines = f.readlines()
+with open('./kernel.h', 'w') as f:
+    #i = 0
+    for i, line in enumerate(lines):
+        #i = i + 1
+        if i ==18:
+            if len(line) != 29:
+                f.write('//'+line)
+            else:
+                f.write(line)
+        else:
+            f.write(line)
+        
