a84da1583e43151261c1f14ac4f3846ec81c9045
TSP <44106649+Spider-scnu@users.noreply.github.com>
2021-06-02 12:26:24 +0800

    Update solve-tsp50.sh

--- MCTS/tsp-20-50-100/solve-tsp50.sh
+++ MCTS/tsp-20-50-100/solve-tsp50.sh
@@ -1,11 +1,34 @@
 #!/usr/bin/env sh
+rm -r kernel.o 
+python ./utils/preprocess.py
+nvcc -L /usr/local/cuda-9.0/lib64/ -I /usr/local/cuda-9.0/samples/common/inc   -lcudart -lcuda  -c kernel.cu
+
+
 rm -r TSP.o
 rm -r test
+clear
+
+python ./utils/preprocess2.py
+
 g++ -lstdc++ -std=c++11 -fPIC -fvisibility=hidden -o  TSP.o -c TSP.cpp  -I/usr/local/cuda-9.0/samples/common/inc
 
 g++ -lstdc++ -std=c++11 -fPIC -o test TSP.o kernel.o -L. -L/usr/local/cuda-9.0/lib64/ -lcudart -lcuda -lpthread -lm -ldl
 
+threads=(1)
+GPUNum=(1)
+CityNum=(50)
+for ((j=0;j<$GPUNum;j++));do
+{
+for ((i=0;i<$threads;i++));do
+{
+	
+	./test $[$[$threads * $j] + $i] ../results/${CityNum}/results_tsp${CityNum}_batch_$[$[$threads * $j] + $i].txt ../instances/tsp${CityNum}_test_concorde.txt ${CityNum} $j
 
-./test ./results/result_50.txt ./tsp50_test_concorde.txt 50
+}&
+done
+wait
+}&
+done
+wait
 
 echo "Done."
